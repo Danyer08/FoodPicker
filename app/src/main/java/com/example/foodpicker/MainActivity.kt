788559d3c1a1dde3406e0bc0a64b9fcab53b0d5f
Danyer <danjer0826@gmail.com>
2019-06-18 15:43:17 -0400

    Added layout

--- app/src/main/java/com/example/foodpicker/MainActivity.kt
+++ app/src/main/java/com/example/foodpicker/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.foodpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectedFoodTxt.text = ""
+        var foods = arrayOf("Burger King", "Mc Donald's", "Papa John's", "Little Caesar", "Bon Ice Cream")
+
+        decideBtn.
+
+        selectedFoodTxt.text = foods.random()
     }
 }
