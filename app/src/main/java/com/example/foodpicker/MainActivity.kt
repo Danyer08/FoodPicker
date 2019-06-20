106457598a3c94631e3d5efd5e5cad3636b9bfa8
Danyer <danjer0826@gmail.com>
2019-06-19 22:51:01 -0400

    Added food price and decider button function.

--- app/src/main/java/com/example/foodpicker/MainActivity.kt
+++ app/src/main/java/com/example/foodpicker/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.foodpicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,11 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = ""
-        var foods = arrayOf("Burger King", "Mc Donald's", "Papa John's", "Little Caesar", "Bon Ice Cream")
+        selectedFoodTxt.text = "Select a food"
+        val foods = arrayListOf(Food("Pizza",800))
 
-        decideBtn.
+        decideBtn.setOnClickListener {
+            var random = Random().nextInt(foods.size)
+            var selectedFood = foods[random]
+            selectedFoodTxt.text = selectedFood.name
+            foodPriceLabel.text = selectedFood.price.toString()
+        }
 
-        selectedFoodTxt.text = foods.random()
+        addFoodBtn.setOnClickListener {
+            val foodName = foodNameTxt.text.toString()
+            val foodPrice = foodPriceTxt.text.toString()
+            val food = Food(name = foodName, price = foodPrice.toInt())
+            foods.add(food)
+            foodNameTxt.text.clear()
+            foodPriceTxt.text.clear()
+        }
     }
 }
+
+data class Food(val name: String, val price: Int)
